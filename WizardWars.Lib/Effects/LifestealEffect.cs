6c101247ff40b1fad3f932fc352c8016875c5f2c
MadsHebsgaard <hebsgaard.mads@gmail.com>
2022-06-23 17:09:23 +0200

    Updated Lifesteal effect

---
+++ WizardWars.Lib/Effects/LifestealEffect.cs
@@ -0,0 +1,12 @@
+﻿namespace WizardWars.Lib.Effects;
+
+public class LifestealEffect : Effect
+{
+	public int LifestealAmount { get; set; }
+
+	public override void Apply(Wizard caster, Wizard target)
+	{
+		target.Health -= LifestealAmount;
+		caster.Health += LifestealAmount;
+	}
+}
