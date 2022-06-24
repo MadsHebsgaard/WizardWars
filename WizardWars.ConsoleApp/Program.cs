ac518ca723922660adb54ba34f10536ed9bacea7
MadsHebsgaard <hebsgaard.mads@gmail.com>
2022-06-24 10:59:45 +0200

    Cleaned up Main

--- WizardWars.ConsoleApp/Program.cs
+++ WizardWars.ConsoleApp/Program.cs
@@ -12,9 +12,8 @@ public static class Program
 
 	public static void Main()
 	{
+		//UI and Spell setup
 		IUserInterface userInterface = new SpectreConsoleUserInterface();
-		
-		//Spell setup
 		var spellsFromJson = ReadFromJson<IEnumerable<Spell>>("spells.json").ToList();
 
 		//Wizard setup
@@ -23,80 +22,45 @@ public static class Program
 		var wizard1 = new Wizard(firstPlayerName);
 		var wizard2 = new Wizard(secondPlayerName);
 
-		//var p2Spells = userInterface.UserPicksSpell(wizard2, spellsFromJson.Where(x => x.Name == "Meditate").ToList());
-
 		//Duel setup
-		Console.WriteLine("\n ------------------------------  ");
-		int maxTurns = 100;
-		int turnNumber = 0;
+		Console.WriteLine();
+		int turnNumber = 0, maxTurns = 100;
 
 		while (wizard1.Health >= 0 && wizard2.Health >= 0 && turnNumber < maxTurns)
-		{
-			turnNumber++;
+		{	turnNumber++;
 
 			//Known spells
 			var p1SpellList = spellsFromJson.Where(x => x.IQRequired <= wizard1.IQ).ToList();
 			var p2SpellList = spellsFromJson.Where(x => x.IQRequired <= wizard2.IQ).ToList();
 
-			Console.WriteLine("Turn: " + turnNumber);
-			Console.WriteLine(" ------------------------------  ");
+			//Stats before casting spells
+			Console.WriteLine($"\n -------------------------------\n  	    Turn: {turnNumber}\n -------------------------------  ");
 			userInterface.DisplayStats(wizard1, wizard2);
 
+			//player 1 and 2 moves.
 			var p1Spell = userInterface.UserPicksSpell(wizard1, p1SpellList.Where(x => x.ManaCost <= wizard1.Mana).ToList());
 			var p1Target = userInterface.UserPicksTarget() == Target.Self ? wizard1 : wizard2;
 			var p1 = new SpellTarget(wizard1, p1Spell, p1Target);
-
 			var p2Spell = userInterface.UserPicksSpell(wizard2, p2SpellList.Where(x => x.ManaCost <= wizard2.Mana).ToList());
 			var p2Target = userInterface.UserPicksTarget() == Target.Self ? wizard2 : wizard1;
 			var p2 = new SpellTarget(wizard2, p2Spell, p2Target);
 
-
-			Console.WriteLine();
+			//Create, execute and show turn
 			var turn = new Turn(p1, p2);
 			turn.Execute();
-
-			wizard1.Mana -= p1Spell.ManaCost;
-			wizard2.Mana -= p2Spell.ManaCost;
-
-
-			//Show events
 			userInterface.DisplaySpellCastInformation(p1);
 			userInterface.DisplaySpellCastInformation(p2);
 
-			Console.WriteLine("\n ------------------------------ ");
-			
-			// TODO: show regen effects
-
+			// TODO: add these to seperate "Regen" function (wizard1, wizard2, spell1, spell2)
 			wizard1.Health += wizard1.HealthRegen;
 			wizard2.Health += wizard1.HealthRegen;
-			wizard1.Mana += wizard1.ManaRegen;
-			wizard2.Mana += wizard1.ManaRegen;
+			wizard1.Mana += wizard1.ManaRegen- p1Spell.ManaCost;
+			wizard2.Mana += wizard1.ManaRegen- p2Spell.ManaCost;
 			wizard1.IQ += wizard1.IQRegen;
 			wizard2.IQ += wizard2.IQRegen;
 		}
-
-		//Text after duel is over:
-		if (turnNumber != maxTurns)
-		{
-			if (wizard1.Health > 0)
-			{
-				Console.WriteLine(wizard1.Name + " killed " + wizard2.Name + " and won the Duel!");
-			}
-			else if (wizard2.Health > 0)
-			{
-				Console.WriteLine(wizard2.Name + " murdered " + wizard1.Name + " and won the Duel!");
-			}
-			else
-			{
-				Console.WriteLine(wizard1.Name + " and " + wizard2.Name + " killed each other and the Duel is lost for both but their honors remain intact!");
-			}
-		}
-		else	
-		{ 
-			Console.WriteLine("Dual over! Ran out of max turns of " + maxTurns); 
-		}
+		userInterface.DisplayWinText(wizard1, wizard2, turnNumber, maxTurns);
 	}
-
 	private static T ReadFromJson<T>(string filename)
 	{
 		return JsonConvert.DeserializeObject<T>(File.ReadAllText(filename), _jsonSerializerSettings); //"Possible null reference return"
