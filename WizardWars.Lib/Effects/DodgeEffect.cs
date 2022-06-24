de41ac6bcd7c20e863287aec7d6e2e4cb131ed67
MadsHebsgaard <hebsgaard.mads@gmail.com>
2022-06-24 09:55:28 +0200

    Added effects and their spells

---
+++ WizardWars.Lib/Effects/DodgeEffect.cs
@@ -0,0 +1,10 @@
+﻿namespace WizardWars.Lib.Effects;
+
+public class BounceEffect : Effect
+{
+	
+	public override void Apply(Wizard caster, Wizard target)
+	{
+		target = caster; //TODO doesn't work
+	}
+}
---
+++ WizardWars.Lib/Effects/ManaStealEffect.cs
@@ -0,0 +1,12 @@
+﻿namespace WizardWars.Lib.Effects;
+
+public class ManaStealEffect : Effect
+{
+	public int ManaSteal { get; set; }
+
+	public override void Apply(Wizard caster, Wizard target)
+	{
+		caster.Mana += ManaSteal;
+		target.Mana -= ManaSteal;
+	}
+}
